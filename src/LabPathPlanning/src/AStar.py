db2fa7682430d3c58b2d7cb11e2e049de5252fb1
kkumar30 <kkumarshivi@gmail.com>
2017-04-04 14:52:01 -0400

    Added A star pseudocode

---
+++ src/LabPathPlanning/src/AStar.py
@@ -0,0 +1,63 @@
+import rospy, tf, numpy, math
+from kobuki_msgs.msg import BumperEvent
+from std_msgs.msg import String
+from geometry_msgs.msg import Twist
+from nav_msgs.msg import Odometry
+from nav_msgs.msg import OccupancyGrid
+from nav_msgs.msg import GridCells
+from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import Pose, PoseWithCovarianceStamped, Point
+from tf.transformations import euler_from_quaternion
+from heapq import *
+#Add additional imports for each of the message types used
+
+
+# Map Values
+# – 100 – Wall
+# – 0 – Empty
+# – -1 – Unknown
+
+function A*(start,goal)
+	#initially only the starting node is known
+	closedSet:={} #Set of evaluated nodes
+
+	openSet:={}
+
+	cameFrom := the empty map
+
+	gCost := map with default value of 100
+
+	gCost[start]:=0
+
+	fCost[start]:=heuristic_cost_estimate(start,goal)
+
+	while openSet is not empty
+		current:= the node in the openSet having the lowest fCost[] value
+
+		if current = goal
+			return reconstruct_path(cameFrom,current)
+
+		openSet.Remove(current)
+		closedSet.Add(current)
+
+
+		for each neighbor of current
+
+			if neighbor in closedSet
+			#Ignore the neighbor- already evaluated
+			continue
+			#dist from start to a neighbour	
+			tentative_gCost: = gCost[current] + dist_between(current, neighbor)
+
+			#if it discovers a new node
+			if neighbor not in openSet
+				openSet.Add(neighbor)
+			else if tentative_gCost >= gCost[neighbour]
+				#This is not a better graph
+				continue
+
+			cameFrom[neighbor] := current
+			gCost[neighbor] := tentative_gCost
+			fCost[neighbor] := gCost[neighbor]
+
+	return failed
