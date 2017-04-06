071a2aac9674735819bf9f8754e717c62833ae84
briggscalum <briggscalum@gmail.com>
2017-04-05 21:08:52 -0400

    I wrote the code

--- src/LabPathPlanning/src/AStar.py
+++ src/LabPathPlanning/src/AStar.py
@@ -9,82 +9,147 @@ from geometry_msgs.msg import PoseStamped
 from geometry_msgs.msg import Pose, PoseWithCovarianceStamped, Point
 from tf.transformations import euler_from_quaternion
 from Queue import PriorityQueue
+import math
+import time
+
+
 #Add additional imports for each of the message types used
 #https://docs.python.org/2/library/heapq.html - Go to this url to see Priority Queue documentation
 
 
 # Map Values
-# – 100 – Wall
-# – 0 – Empty
-# – -1 – Unknown
+#  100  Wall
+#  0  Empty
+#  -1  Unknown
 
-function A*(start_x, start_y,goal_x, goal_y)
-	#initially only the starting node is known
-	closedSet = PriorityQueue() #Set of evaluated nodes, makes it a heapSort
+def findhCost(current_pose, goal_pose, graph):
 
-##MAIN PSEUDOCODE STARTS HERE 
-	openSet = Queue()
+	return math.sqrt((current_pose[0] - goal_pose[0])**2 + (current_pose[1] - goal_pose[1])**2)
 
-	cameFrom := the empty map
+def findfCost(current_pose, start_pose, graph):
 
-	gCost := map with default value of 100
+	return abs((current_pose[0] - start_pose[0]) + abs(current_pose[1] - start_pose[1]))
 
-	gCost[start]:=0
+def findCost(current_pose, start_pose, goal_pose, graph):
 
-	fCost[start]:=heuristic_cost_estimate(start,goal)
+	if graph[current_pose[0]][current_pose[1]] == 0:
 
-	while openSet is not empty
-		current:= the node in the openSet having the lowest fCost[] value
+		return (findhCost(current_pose, goal_pose, graph) + findfCost(current_pose, start_pose, graph))
+	else :
+		return graph[current_pose[0]][current_pose[1]]
 
-		if current = goal
-			return reconstruct_path(cameFrom,current)
-
-		openSet.Remove(current)
-		closedSet.Add(current)
+def Astar(start_pose, goal_pose, graph):
 
 
-		for each neighbor of current
+	frontNodes = [] #Frontier nodes
+	checkedNodes = [] #Checked nodes
+	checkedNodes.append(start_pose)
+	
+	graph	
 
-			if neighbor in closedSet
-			#Ignore the neighbor- already evaluated
-			continue
-			#dist from start to a neighbour	
-			tentative_gCost: = gCost[current] + dist_between(current, neighbor)
+	nodestocheck = [] # Priority queue of Frontier
 
-			#if it discovers a new node
-			if neighbor not in openSet
-				openSet.Add(neighbor)
-			else if tentative_gCost >= gCost[neighbour]
-				#This is not a better graph
-				continue
+	path = [] # List of nodes to move 
+	
+	goalfound = False
 
-			cameFrom[neighbor] := current
-			gCost[neighbor] := tentative_gCost
-			fCost[neighbor] := gCost[neighbor]
+	current_pose = start_pose # Start searching at the start point
 
-	return failed
+	graph[current_pose[0]][current_pose[1]] = findhCost(current_pose, goal_pose, graph) + findfCost(current_pose, start_pose, graph)
 
+	checked = False
 
+	while not goalfound: # This loop finds all needed node costs until it finds the goal
+	
+		print current_pose
+		temp_node = [current_pose[0] + 1, current_pose[1]]
+		checked = False
+		for node in checkedNodes:
+			if node[0] == temp_node[0] and  node[1] == temp_node[1] or temp_node[0] < 0 or temp_node[1] < 0 or temp_node[0] > 4 or temp_node[1] > 4:
+				checked = True
+		if checked == False and graph[temp_node[0]][temp_node[1]] != 100:
+			graph[temp_node[0]][temp_node[1]] = findhCost(temp_node, goal_pose, graph) + findfCost(temp_node, start_pose, graph)
+			findCost(temp_node, start_pose, goal_pose, graph)
+			frontNodes.append(temp_node)
+			temp_node.append(current_pose) #node
+			checkedNodes.append(temp_node)
+				
+		
+		temp_node = [current_pose[0] - 1, current_pose[1]]
+		checked = False
+		for node in checkedNodes:	
+			if node[0] == temp_node[0] and  node[1] == temp_node[1] or temp_node[0] < 0 or temp_node[1] < 0 or temp_node[0] > 4 or temp_node[1] > 4:
+				checked = True
+		if checked == False and graph[temp_node[0]][temp_node[1]] != 100:
+			graph[temp_node[0]][temp_node[1]] = findhCost(temp_node, goal_pose, graph) + findfCost(temp_node, start_pose, graph)		
+			findCost(temp_node, start_pose, goal_pose, graph)
+			frontNodes.append(temp_node)
+			temp_node.append(current_pose)
+			checkedNodes.append(temp_node)
+			
+		temp_node = [current_pose[0], current_pose[1] + 1]
+		
+		checked = False		
+		for node in checkedNodes:
+			if node[0] == temp_node[0] and  node[1] == temp_node[1] or temp_node[0] < 0 or temp_node[1] < 0 or temp_node[0] > 4 or temp_node[1] > 4:
+				checked = True
+		
+		if checked == False and graph[temp_node[0]][temp_node[1]] != 100:
+			graph[temp_node[0]][temp_node[1]] = findhCost(temp_node, goal_pose, graph) + findfCost(temp_node, start_pose, graph)
+			findCost(temp_node, start_pose, goal_pose, graph)
+			frontNodes.append(temp_node)
+			temp_node.append(current_pose)
+			checkedNodes.append(temp_node)
 
-def talker():
-    global pub
-    rospy.init_node('LabPathPlanning')        
-    sub = rospy.Subscriber("/map", OccupancyGrid, simpleMapCallback)
-    pub = rospy.Publisher("/myGridCells", GridCells, queue_size=1)
-    rospy.sleep(1)
+		
+		temp_node = [current_pose[0], current_pose[1] - 1]
+		
+		checked = False		
+		for node in checkedNodes:
+			if node[0] == temp_node[0] and  node[1] == temp_node[1] or temp_node[0] < 0 or temp_node[1] < 0 or temp_node[0] > 4 or temp_node[1] > 4:
+				checked = True
+		if checked == False and graph[temp_node[0]][temp_node[1]] != 100:
+			graph[temp_node[0]][temp_node[1]] = findhCost(temp_node, goal_pose, graph) + findfCost(temp_node, start_pose, graph)
+			findCost(temp_node, start_pose, goal_pose, graph)
+			frontNodes.append(temp_node)
+			temp_node.append(current_pose)
+			checkedNodes.append(temp_node)
 
-    while (1 and not rospy.is_shutdown()):
-        publishGridCells(mapData) #publishing map data every 2 seconds
-        rospy.sleep(2)  
-        print("Complete")
+		
+		bestnode = frontNodes[0]
+		for node in frontNodes:
+			if graph[node[0]][node[1]] <= graph[bestnode[0]][bestnode[1]]:
+				bestnode = node
+		frontNodes.remove(bestnode)
+		current_pose = [bestnode[0], bestnode[1]]
+		
+		
+		
 
-# This is the program's main function
-if __name__ == '__main__':
-    # Change this node name to include your username
-    rospy.init_node('LabPathPlanning')
-    #These are global variables. Write "global <variable_name>" in any other function to gain access to these global variables
+		if bestnode[0] == goal_pose[0] and  bestnode[1] == goal_pose[1]:
+			goalfound = True
+		
+	
+	current_pose = bestnode	
+	path.append(current_pose)
+	startfound = False
+	while not startfound: # This loop finds the path throught the checkes nodes
+		
+		path.append(current_pose)
+		if current_pose == start_pose:
+			startfound = True
+			break
+		for node in checkedNodes:
+			if node[0] == current_pose[2][0] and  node[1] == current_pose[2][1]:
+				current_pose = node
+				break
+	
+	nodepath = []
+	for i in range (1,len(path)):
+		nodepath.append([path[len(path)-i][0],path[len(path)-i][1]])
 
-    try:
-        talker()
-    except rospy.ROSInterruptException:
-        pass
+	
+		
+		
+
+	return nodepath
